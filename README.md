012a2457f17cd74d57c49cae8ddc095573fcf87a
Philipp Waldhauer <ich@philippwaldhauer.de>
2014-08-03 00:41:29 +0200

    Added functionality

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# psdtest
+# daiquiri-psd
 
-Please write a meaningful readme.
+Takes an pds file as input and outputs the needed JSON to generate the gallery using [daiquiri](https://github.com/pwaldhauer/daiquiri)
+
+## Usage
+
+- Create your gallery layout in Photoshop. Wrap images that should be shown fullscreen in a group named "full" and images that should be displayed in a grid in a "grid" group.
+- `daiquiri-psd -o gallery.json gallery.psd`
+- Done!
+
+(Please take care, this script replaces the whole `sections` block of your json)
+
+## Todo
+
+- Support for text blocks
--- app.js
+++ app.js
@@ -1,3 +1,79 @@
+#!/usr/bin/env node
+
 var _ = require('underscore');
 var async = require('async');
+var fs = require('fs');
 
+var argv = require('yargs')
+    .usage('Usage: $0 -o [path-to-json] [path-to-psd]')
+    .demand(1)
+    .demand(['o'])
+    .argv;
+
+var PSD = require('psd');
+var path = require('path');
+var psd = PSD.fromFile(path.resolve(argv._[0]));
+
+var group_types = ['full', 'grid'];
+var sections = [];
+
+psd.parse();
+
+var all_sections = _.sortBy(psd.tree().children(), function(section) {
+    return -1 * section.top;
+});
+
+_.each(all_sections, function(section) {
+
+    if(section.type != 'group' || section.name != 'section' || section.children().length == 0) {
+        console.log('Skipping non-group: ' + section.name);
+        return;
+    }
+
+    var all_boxes =  _.sortBy(section.children(), function(box) {
+        return -1 * box.top;
+    });
+
+    var boxes = [];
+
+    _.each(all_boxes, function(box) {
+        if(box.type != 'group' || !_.contains(group_types, box.name) || box.children().length === 0) {
+            console.log('Ignoring group: ' + box.name);
+            return;
+        }
+
+        var all_children = _.sortBy(box.children(), function(layer) {
+            return -1 * layer.left
+        });
+
+        var files = [];
+        _.each(all_children, function(child) {
+
+            if(child.type != 'layer') {
+                console.log('Skipping nested group: ' + child.name);
+                return;
+            }
+
+            files.push(child.name);
+        })
+
+        boxes.push({
+            type: box.name,
+            media: files
+        });
+    })
+
+    sections.push({
+        'title': 'Section',
+        'boxes': boxes
+    });
+
+});
+
+
+var output = require(path.resolve(argv.o));
+output.sections = sections;
+
+fs.writeFileSync(argv.o, JSON.stringify(output, null, '\t'));
+
+console.log('Done, exported ' + sections.length + ' sections to ' + argv.o);
