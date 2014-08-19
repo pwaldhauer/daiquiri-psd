54fd25e14a614fa0be3a4a7dfb4a7b1437c2f31b
Philipp Waldhauer <ich@philippwaldhauer.de>
2014-08-19 16:02:00 +0200

    Added parsing of text layers. Thanks to @imgqb who implemented the text layer parsing in psd.js. :)

--- README.md
+++ README.md
@@ -10,21 +10,30 @@ Takes an pds file as input and outputs the needed JSON to generate the gallery u
 
 ## Usage
 
-- Create your gallery layout in Photoshop:
- - Wrap your sections in groups named `section`
- - Wrap images that should be shown fullscreen in a group named `full` and images that should be displayed in a grid in a `grid` group.
- - You don't need to order the layers/groups. Ordering will be done automatically by the y value of layers from top to bottom.
+- Create your gallery layout in Photoshop, it should look like this. See the available block types below.
 
-![Screenshot](https://s3-eu-west-1.amazonaws.com/knusperfiles/daiquiri-psd-screen.png)
+![Screenshot](https://s3-eu-west-1.amazonaws.com/knusperfiles/daiquiri-psd-screen_2.png)
+
+- You don't need to order the layers/groups. Ordering will be done automatically by the y value of layers from top to bottom.
 
 - Run `daiquiri-psd -o gallery.json gallery.psd`
 - Done!
 
 (Please take care, this script replaces the whole `sections` block of your json)
 
-## Todo
+## Block types
 
-- Support for text blocks
+| Name       | Description                                                              |
+|------------|--------------------------------------------------------------------------|
+| `header`   | Section containing headline, subtitle and header image                   |
+| `headline` | Should contain exactly one text layer, used for the headline             |
+| `subtitle` | Should contain exactly one text layer, used for the subtitle             |
+| `image`    | Should contain exactly one image layer, used for the background image    |
+| `section`  | A default section                                                        |
+| `title`    | Should contain exactly one text layer, used for the title of the section |
+| `text`     | Can contain multiple text layers, which translate to paragraphs          |
+| `grid`     | Can contain one or more images that will be layed out in a grid          |
+| `full`     | Should contain exactly one image, that will be layed out in full size    |
 
 ## License
 
--- app.js
+++ app.js
@@ -16,7 +16,13 @@ var PSD = require('psd');
 var path = require('path');
 var psd = PSD.fromFile(path.resolve(argv._[0]));
 
-var group_types = ['full', 'grid'];
+var section_types = ['header', 'section'];
+var group_types = ['title', 'text', 'full', 'grid', 'headline', 'subtitle', 'image'];
+var title = {
+    "headline": null,
+    "subtitle": null,
+    "media": null
+};
 var sections = [];
 
 psd.parse();
@@ -26,12 +32,53 @@ var all_sections = _.sortBy(psd.tree().children(), function(section) {
 });
 
 _.each(all_sections, function(section) {
+    var current_section_title = 'Section title';
 
-    if(section.type != 'group' || section.name != 'section' || section.children().length == 0) {
+    if(section.type != 'group' || !_.contains(section_types, section.name) || section.children().length == 0) {
         console.log('Skipping non-group: ' + section.name);
         return;
     }
 
+    // Parse header
+    if(section.name == 'header') {
+         var all_boxes =  _.sortBy(section.children(), function(box) {
+             return box.top;
+         });
+
+         _.each(all_boxes, function(box) {
+             if(box.type != 'group' || !_.contains(group_types, box.name) || box.children().length === 0) {
+                 console.log('Ignoring group: ' + box.name + '(' + box.type + ')');
+
+                 return;
+             }
+
+             if(box.name == 'headline') {
+                 if(_.isUndefined(box.children()[0].get('typeTool'))) {
+                         return;
+                 }
+
+                 title.headline = box.children()[0].get('typeTool').textData['Txt '];
+                 return;
+             }
+
+             if(box.name == 'subtitle') {
+                 if(_.isUndefined(box.children()[0].get('typeTool'))) {
+                         return;
+                 }
+
+                 title.subtitle = box.children()[0].get('typeTool').textData['Txt '];
+                 return;
+             }
+
+             if(box.name == 'image') {
+                title.media = [box.children()[0].name];
+             }
+         });
+
+        return;
+    }
+
+    // Parse other boxes
     var all_boxes =  _.sortBy(section.children(), function(box) {
         return box.top;
     });
@@ -49,9 +96,39 @@ _.each(all_sections, function(section) {
             return layer.left
         });
 
+        // Section title
+        if(box.name == 'title') {
+            if(_.isUndefined(all_children[0].get('typeTool'))) {
+                    return;
+            }
+
+            current_section_title = all_children[0].get('typeTool').textData['Txt '];
+            return;
+        }
+
+        // Text boxes
+        if(box.name == 'text') {
+            var paragraphs = [];
+            _.each(all_children, function(child) {
+                if(_.isUndefined(child.get('typeTool'))) {
+                    return;
+                }
+
+                // Yes, there needs to be a space 8-)
+                paragraphs.push(child.get('typeTool').textData['Txt ']);
+            })
+
+            boxes.push({
+                type: 'text',
+                paragraphs: paragraphs
+            });
+
+            return;
+        }
+
+        // Image boxes
         var files = [];
         _.each(all_children, function(child) {
-
             if(child.type != 'layer') {
                 console.log('Skipping nested group: ' + child.name);
                 return;
@@ -68,7 +145,7 @@ _.each(all_sections, function(section) {
     })
 
     sections.push({
-        'title': 'Section',
+        'title': current_section_title,
         'boxes': boxes
     });
 
@@ -77,6 +154,18 @@ _.each(all_sections, function(section) {
 
 var output = require(path.resolve(argv.o));
 output.sections = sections;
+
+if(title.headline != null) {
+    output.meta.title.headline = title.headline;
+}
+
+if(title.subtitle != null) {
+    output.meta.title.subtitle = title.subtitle;
+}
+
+if(title.media != null) {
+    output.meta.title.media = title.media;
+}
 
 fs.writeFileSync(argv.o, JSON.stringify(output, null, '\t'));
 
